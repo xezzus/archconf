6d6e6b7d5750f2f94337e874354f6c01b9b3d8fa
Andrey Lisovoy <xezzus@gmail.com>
2013-01-10 21:57:03 +0400

    Correct

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# INSTALL
+pacman -Syu && pacman -S gvim terminus-font
+
 # COPY CONFIG FILES
 cp ./files/fstab /etc
 cp ./files/locale.gen /etc
+cp ./files/locale.conf /etc
+cp ./files/vconsole.conf /etc
+cp ./files/mirrorlist /etc/pacman.d
 
 # COMMAND
 locale-gen
