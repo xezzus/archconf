0e5ec102b0bb82cd0a2a71916ac38c13a18400e4
Andrey Lisovoy <xezzus@gmail.com>
2013-01-11 13:01:09 +0400

    Correct

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ archconf
 ```shell
 mkfs.ext4 -O dir_index,extent,sparse_super /dev/hdd
 ```
+
+```shell
+xrand
+```
--- install.sh
+++ install.sh
@@ -2,16 +2,27 @@
 
 # DATA
 user=web
+conf=archconf
 
 # INSTALL
 cp ./files/mirrorlist /etc/pacman.d
-pacman -Syy && pacman -Syu && pacman -S gvim terminus-font wicd sudo mc
+pacman -Syy && pacman -Syu && pacman -S gvim terminus-font wicd sudo mc xorg-server dmenu dzen2 chromium sakura slim base-devel abs
 
-# CONFIG
+# DWM
+abs community/dwm
+cp /var/abs/community/dwm ~/dwm
+cp ./files/dwm.c ~/dwm
+cp ./files/config.h ~/dwm
+cd ~/dwm && makepkg -i && cd ~/$archconf
+
+# CONFIG ROOT
 cp ./files/fstab /etc
 cp ./files/locale.conf /etc
 cp ./files/vconsole.conf /etc
 cp ./files/sudoers /etc
+
+# CONFIG USER
+cp ./files/xinitrc /home/$user/.xinitrc
 
 # COMMAND
 if [ -z `locale | grep 'LANG=ru_RU'` ]
