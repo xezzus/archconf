65393f611055cae7cc203c49cb308c448a6d4960
Andrey Lisovoy <xezzus@gmail.com>
2013-01-11 11:33:29 +0400

    Correct

--- install.sh
+++ install.sh
@@ -23,6 +23,12 @@ fi
 # ADD USERS
 if [ -z `cat /etc/passwd | grep $user` ] 
 then
-  useradd -m -g users -G audio,games,lp,optical,power,scanner,storage,video -s /bin/bash $user
+  useradd -m -g users -G audio,games,lp,optical,power,scanner,storage,video,network -s /bin/bash $user
   passwd && passwd $user
+fi
+
+# ADD SERVICES
+if [ -z `systemctl | grep wicd` ]
+then
+  systemctl enable wicd.service
 fi
