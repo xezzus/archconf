e7f6e8d5032f5d49d2533c5c3a2359cd80c88cd1
Andrey Lisovoy <xezzus@gmail.com>
2013-01-11 10:10:39 +0400

    Correct

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 user=web
 
 # INSTALL
@@ -7,14 +8,6 @@ pacman -Syy && pacman -Syu && pacman -S gvim terminus-font wicd sudo
 # ADD USERS
 useradd -m -g users -G audio,games,lp,optical,power,scanner,storage,video -s /bin/bash $user
 passwd && passwd $user
-
-# COPY CONFIG FILES
-cp ./files/fstab /etc
-cp ./files/locale.gen /etc
-cp ./files/locale.conf /etc
-cp ./files/vconsole.conf /etc
-cp ./files/sudoers /etc
-cp ./files/mirrorlist /etc/pacman.d
 
 # COMMAND
 locale-gen
