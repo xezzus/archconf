d61664e5b4cd8f2796185d89fec066ec567f12bd
Andrey Lisovoy <xezzus@gmail.com>
2013-01-12 04:20:46 +0400

    Correct

--- bind.sh
+++ bind.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ `pacman -Qi | grep 'bind' | wc l` -eq 0 ] ; then pacman -S bind ; fi
-if [ `pacman -Qi | grep 'dnsutils' | wc l` -eq 0 ] ; then pacman -S dnsutils ; fi
+if [ `pacman -Qi | grep 'bind' | wc -l` -eq 0 ] ; then pacman -S bind ; fi
+if [ `pacman -Qi | grep 'dnsutils' | wc -l` -eq 0 ] ; then pacman -S dnsutils ; fi
 
 # INSTALL BIND
 cp ./bind/named.conf.local ./
