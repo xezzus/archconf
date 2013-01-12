3c036652dff12a8b4fedf1107907bcc78922a93a
Andrey Lisovoy <xezzus@gmail.com>
2013-01-12 16:40:11 +0400

    Correct

--- install.sh
+++ install.sh
@@ -11,7 +11,9 @@ pacman -Syy && pacman -Syu
 # INSTALL
 if [ `pacman -Qi lilo | wc -l` -eq 0 ] ; then $install lilo ; fi
 if [ `pacman -Qi terminus-font | wc -l` -eq 0 ] ; then $install terminus-font ; fi
-if [ `pacman -Qi wicd | wc -l` -eq 0 ] ; then $install wicd ; fi
+if [ `pacman -Qi netcfg | wc -l` -eq 0 ] ; then $install netcfg ; fi
+if [ `pacman -Qi wpa_actiond | wc -l` -eq 0 ] ; then $install wpa_actiond ; fi
+if [ `pacman -Qi ifplugd | wc -l` -eq 0 ] ; then $install ifplugd ; fi
 if [ `pacman -Qi sudo | wc -l` -eq 0 ] ; then $install sudo ; fi
 
 # CONFIG ROOT
@@ -19,6 +21,8 @@ cp ./files/fstab /etc
 cp ./files/locale.conf /etc
 cp ./files/vconsole.conf /etc
 cp ./files/sudoers /etc
+cp ./files/netcfg /etc/conf.d
+cp ./files/dhcp /etc/network.d
 
 # COMMAND
 if [ -z `locale | grep 'LANG=ru_RU'` ]
@@ -35,7 +39,12 @@ then
 fi
 
 # ADD SERVICES
-if [ -z `systemctl | grep 'wicd.service' | awk '{print($1)}'` ]
+if [ -z `systemctl | grep 'netcfg.service' | awk '{print($1)}'` ]
 then
-  systemctl enable wicd.service
+  systemctl enable netcfg.service
+fi
+
+if [ -z `systemctl | grep 'net-auto-wired.service' | awk '{print($1)}'` ]
+then
+  systemctl enable net-auto-wired.service
 fi
