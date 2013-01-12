fe7cb97bd8374b2b91eece1d1db34f1953b07aff
Andrey Lisovoy <xezzus@gmail.com>
2013-01-12 05:17:43 +0400

    Correct

--- server.sh
+++ server.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+# METHOD INSTALL
+install='pacman --noconfirm -S'
+
 # INSTALL BIND
-if [ `pacman -Qi | grep 'bind' | wc -l` -eq 0 ] ; then pacman -S bind ; fi
-if [ `pacman -Qi | grep 'dnsutils' | wc -l` -eq 0 ] ; then pacman -S dnsutils ; fi
+if [ `pacman -Qi | grep 'bind' | wc -l` -eq 0 ] ; then $install bind ; fi
+if [ `pacman -Qi | grep 'dnsutils' | wc -l` -eq 0 ] ; then $install dnsutils ; fi
 cp ./files/dev.zone /var/named && chown root:named /var/named/dev.zone
 
 # INSTALL NGINX
-if [ `pacman -Qi | grep 'nginx' | wc -l` -eq 0 ] ; then pacman -S nginx ; fi
+if [ `pacman -Qi | grep 'nginx' | wc -l` -eq 0 ] ; then $install nginx ; fi
 cp ./files/nginx.conf /etc/nginx
 
 # INSTALL PHP
-if [ `pacman -Qi | grep 'php' | wc -l` -eq 0 ] ; then pacman -S php ; fi
-if [ `pacman -Qi | grep 'php-fpm' | wc -l` -eq 0 ] ; then pacman -S php-fpm ; fi
-if [ `pacman -Qi | grep 'php-gd' | wc -l` -eq 0 ] ; then pacman -S php-gd ; fi
-if [ `pacman -Qi | grep 'php-mcrypt' | wc -l` -eq 0 ] ; then pacman -S php-mcrypt ; fi
-if [ `pacman -Qi | grep 'php-pear' | wc -l` -eq 0 ] ; then pacman -S php-pear ; fi
-if [ `pacman -Qi | grep 'php-pgsql' | wc -l` -eq 0 ] ; then pacman -S php-pgsql ; fi
-if [ `pacman -Qi | grep 'php-sqlite' | wc -l` -eq 0 ] ; then pacman -S php-sqlite ; fi
-if [ `pacman -Qi | grep 'php-xsl' | wc -l` -eq 0 ] ; then pacman -S php-xsl ; fi
-pecl install mongo
+if [ `pacman -Qi | grep 'php' | wc -l` -eq 0 ] ; then $install php ; fi
+if [ `pacman -Qi | grep 'php-fpm' | wc -l` -eq 0 ] ; then $install php-fpm ; fi
+if [ `pacman -Qi | grep 'php-gd' | wc -l` -eq 0 ] ; then $install php-gd ; fi
+if [ `pacman -Qi | grep 'php-mcrypt' | wc -l` -eq 0 ] ; then $install php-mcrypt ; fi
+if [ `pacman -Qi | grep 'php-pear' | wc -l` -eq 0 ] ; then $install php-pear ; fi
+if [ `pacman -Qi | grep 'php-pgsql' | wc -l` -eq 0 ] ; then $install php-pgsql ; fi
+if [ `pacman -Qi | grep 'php-sqlite' | wc -l` -eq 0 ] ; then $install php-sqlite ; fi
+if [ `pacman -Qi | grep 'php-xsl' | wc -l` -eq 0 ] ; then $install php-xsl ; fi
+if [ `pecl list | grep mongo | wc -l` -eq 0 ] : then pecl install mongo ; fi
+cp ./files/php.ini /etc/php
+cp ./files/php-fpm.conf /etc/php
