78074f0b5ec1f05e05889baed0bfab74321ac3b7
Andrey Lisovoy <xezzus@gmail.com>
2013-01-12 04:09:20 +0400

    Correct

--- bind.sh
+++ bind.sh
@@ -4,6 +4,6 @@ if [ -z `pacman -Qi | grep ': bind'`] ; then pacman -S bind ; fi
 if [ -z `pacman -Qi | grep ': dnsutils'`] ; then pacman -S dnsutils ; fi
 
 # INSTALL BIND
-cp ./files/named.conf.local ./
-cp ./files/named.conf.options ./
-cp ./files/db.dev ./
+cp ./bind/named.conf.local ./
+cp ./bind/named.conf.options ./
+cp ./bind/db.dev ./
